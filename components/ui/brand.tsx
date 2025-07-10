c34538237036d0a480c33392ce50314b53296b81
Sterling-Credit <sterlingcreditai@gmail.com>
2025-07-10 03:47:23 +0000

    Add stripe dependency

--- components/ui/brand.tsx
+++ components/ui/brand.tsx
@@ -3,17 +3,16 @@
 import Link from "next/link"
 import { FC } from "react"
 
-
 interface BrandProps {
   theme?: "dark" | "light"
 }
 
 export const Brand: FC<BrandProps> = ({ theme = "dark" }) => {
- return (
-    <div className="w-full flex justify-center">
-      <div className="text-4xl font-bold tracking-wide text-center">
+  return (
+    <div className="flex w-full justify-center">
+      <div className="text-center text-4xl font-bold tracking-wide">
         Sterling Credit AI
       </div>
     </div>
-  );
-};
+  )
+}
